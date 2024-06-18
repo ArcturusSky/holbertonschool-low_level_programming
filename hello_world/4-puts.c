92158dd5ac1af5f531357b6eab950f26f767151e
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-18 13:50:47 +0200

    betty essai

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,13 +7,6 @@
  */
 int main(void) 
 {
-	/**
-	 * puts - print a string using puts
-	 *
-	 * Description : print "\"Programming is like building a multilingual puzzle"
-	 *
-	 * Return: 0
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

7c14608db5bd56542068f73084e18c4c055141bf
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-18 13:44:09 +0200

    S3P1T4-CodeCbetty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-int main(void)
+
+/**
+ * main - Entry point
+ *
+ * Description: start the function
+ */
+int main(void) 
 {
+	/**
+	 * puts - print a string using puts
+	 *
+	 * Description : print "\"Programming is like building a multilingual puzzle"
+	 *
+	 * Return: 0
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

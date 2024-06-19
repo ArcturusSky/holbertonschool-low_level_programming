48c22ee2e17fe5afa3a7368540c558c302b95647
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-19 14:41:13 +0200

    S3P2T2-premier_essai

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 /**
- * main - This is the entry point of the code
- * Return: 0 Successful
+ * lastD - Last digit of n
+ * @n: the number to pick the last digit from
  *
+ * Description: function to pick up last digit of n
+ * Return: the last digit of n
  */
 
+int lastD(int n)
+	{
+		return (n % 10);
+	}
+/**
+* main - This is the entry point of the code
+* Return: 0 Successful
+*
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    int lastDigit(int n)
-    {
-        return (n % 10);
-    }
-    if (n > 5)
+
+	if (n > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lasDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastD(n));
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lasDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastD(n));
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lasDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastD(n));
 	}
-    return (0);
-    }
+	return (0);
+	}

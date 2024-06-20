817c67fb309eee3d58056821368286d48ed9bad8
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-20 17:04:57 +0200

    S3P3T1_quel bazar _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - This is the entry point of the code
+* Return: 0 Successful
+*
+*/
+
+int main(void)
+{
+	char t[] = "_putchar";
+	int i = 0;
+while (t[i] != '\0')
+
+{
+	_putchar(t[i]);
+	i++;
+}
+
+_putchar('\n');
+
+return (0);
+}

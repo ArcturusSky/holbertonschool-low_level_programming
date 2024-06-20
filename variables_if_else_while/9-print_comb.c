1478c109e998855db23aa09582413e50801cb4c0
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-20 13:59:07 +0200

    compile

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - This is the entry point of the code
- * Return: 0 Successful
- */
+* main - This is the entry point of the code
+* Return: 0 Successful
+*/
 
 int main(void)
 {
-	for (int n = '0'; n <= '9'; n++)
+	int n;
+
+	for (n = 0; n <= 9; n++)
 	{
 		putchar(n);
-		if (n <= '8')
+		if (n <= 8)
 		putchar(',');
 		putchar(' ');
 	}

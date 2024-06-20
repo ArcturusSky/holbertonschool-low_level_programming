633fc10aa39d3637e74f22e01d728a104b3158ee
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-20 13:46:05 +0200

    S3P2T9- combo j ai galéré un moment, première boucle "for" pour moi.

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ * Return: 0 Successful
+ */
+
+int main(void)
+{
+	for (int n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n <= '8')
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

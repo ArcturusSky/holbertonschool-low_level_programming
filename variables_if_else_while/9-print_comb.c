9a0bcbeaca40192a295aa2c552d98e5799b0b661
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-20 13:55:32 +0200

    test

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - This is the entry point of the code
- * Return: 0 Successful
- */
+* main - This is the entry point of the code
+* Return: 0 Successful
+*/
 
 int main(void)
 {
+	int n = '0'
+
 	for (int n = '0'; n <= '9'; n++)
+
 	{
 		putchar(n);
 		if (n <= '8')

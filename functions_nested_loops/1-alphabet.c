194cca64bf2ca282137ba3a753728dccb81c0c95
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-21 10:59:39 +0200

    S3P3T01- je comprends pas grand chose

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,32 @@
 #include "main.h"
 
 /**
-* main - This is the entry point of the code
-* Return: 0 Successful
-*
-*/
+ * main - Point d'entrée du code, début de la fonction
+ * PRINCIPALE qui va appeler la fonction secondaire ensuite.
+ * Return: 0 Successful
+ *
+ */
 
 int main(void)
 {
-	char az = 'a';
 
-	while (az <= 'z')
-{
-	_putchar(az);
-	az++;
-	}
-	_putchar('\n');
-	return (0);
+/**
+ * print_alphabet - fonction UTILITAIRE pour imprimer l'alphabet
+ * Return: rien ici puisque que c'est "void" print_alphabet pas int
+ *
+ */
+
+	void print_alphabet(void)
+	{
+		char az = 'a';
+
+		while (az <= 'z')
+		{
+			_putchar(az);
+			az++;
+			}
+			_putchar('\n');
+			return;
+			}
+return (0);
 }

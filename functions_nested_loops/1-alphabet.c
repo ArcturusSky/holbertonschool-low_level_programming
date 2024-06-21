3f409f7d4af6d2730645f227b3f92d73140b918c
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-21 11:26:12 +0200

    S3P3T01- ENFIN je crois avoir compris

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,32 +1,19 @@
 #include "main.h"
-
-/**
- * main - Point d'entrée du code, début de la fonction
- * PRINCIPALE qui va appeler la fonction secondaire ensuite.
- * Return: 0 Successful
- *
- */
-
-int main(void)
-{
-
 /**
  * print_alphabet - fonction UTILITAIRE pour imprimer l'alphabet
  * Return: rien ici puisque que c'est "void" print_alphabet pas int
  *
  */
 
-	void print_alphabet(void)
-	{
-		char az = 'a';
+void print_alphabet(void)
+{
+	char az = 'a';
 
 		while (az <= 'z')
 		{
 			_putchar(az);
 			az++;
-			}
-			_putchar('\n');
-			return;
-			}
-return (0);
+		}
+		_putchar('\n');
+		return;
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
+	/**Fonction utilitaire servant à remplacer putchar classique */
 
 void print_alphabet(void);
+	/** Fonction utilitaire servant à imprimer l'alphabet en minuscule */
+#endif

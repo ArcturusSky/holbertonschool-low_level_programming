0de94e5126c2cdca255b47dc6159defbe4975519
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-21 10:19:02 +0200

    S3P3T01- comme char?

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - This is the entry point of the code
+* Return: 0 Successful
+*
+*/
+
+int main(void)
+{
+	char az = 'a';
+
+	while (az <= 'z')
+{
+	_putchar(az);
+	az++;
+	}
+	_putchar('\n');
+	return (0);
+}

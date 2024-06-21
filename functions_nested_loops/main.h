4ca53196c66d32a28478556a75b84c75ffd4c9cf
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-21 13:38:32 +0200

    S3P3T02-pas besoin de pointers pour le moment. Mais il faut que je comprenne mieux les boucles

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,4 +6,7 @@ int _putchar(char c);
 
 void print_alphabet(void);
 	/** Fonction utilitaire servant à imprimer l'alphabet en minuscule */
+
+void print_alphabet_x10(void);
+	/** Fonction utilitaire servant à imprimer l'alphabet en minuscule 10 fois */
 #endif

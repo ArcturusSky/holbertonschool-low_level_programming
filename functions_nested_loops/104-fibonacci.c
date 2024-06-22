4b5835061f7a1c4584098241d22a718c61ec45e1
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-22 20:58:02 +0200

    test avec un nombre en moins encore car l'outpout du Checker n'est pas content

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -15,7 +15,7 @@ c = (a + b);
 compteur = 0;
 
 printf("%lu, %lu, ", a, b);
-while (compteur != 97)
+while (compteur != 96)
 {
 	printf("%lu, ", c);
 	a = b;
@@ -23,7 +23,7 @@ while (compteur != 97)
 	c = (b + a);
 	compteur++;
 }
-if (compteur == 97)
+if (compteur == 96)
 {
 	printf("%lu", c);
 	a = b;

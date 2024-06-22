9cde923957860b9c0ca8789c63afebea8bb06611
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-22 21:01:15 +0200

    on enlève encore une suite ... je comprends pas trop le soucis là.

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -15,7 +15,7 @@ c = (a + b);
 compteur = 0;
 
 printf("%lu, %lu, ", a, b);
-while (compteur != 96)
+while (compteur != 95)
 {
 	printf("%lu, ", c);
 	a = b;
@@ -23,7 +23,7 @@ while (compteur != 96)
 	c = (b + a);
 	compteur++;
 }
-if (compteur == 96)
+if (compteur == 95)
 {
 	printf("%lu", c);
 	a = b;

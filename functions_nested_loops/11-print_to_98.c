7fcd6beb9f40175596ac981c1dddaef89eccd5fc
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-22 18:48:06 +0200

    S3P3T11 -J'ai retiré la limite de digit, j'ai mis "si différent de 98" avec ensuite tant que plus bas faire ++ tant que plus haut faire -- mais si il atteint 98 alors print sans la virgule.

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,24 +9,27 @@
 
 void print_to_98(int n)
 {
-	if (n < 99)
-		while (n < 99)
+	if (n != 98)
+	{
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
-			}
+			if (n == 98)
+			printf("%d", n);
+		}
 
-	else if (n > 97)
-		while (n > 97)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
-			}
+			if (n == 98)
+			printf("%d", n);
+		}
+	}
 	else
 	{
 		printf("%d", n);
-		}
-
+	}
 	printf("\n");
-
 }

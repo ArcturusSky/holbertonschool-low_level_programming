18917abcf792619b9089cc9a3287550ef2d5f7bb
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-22 19:55:50 +0200

    S3P3Tavancée-104 ai-je réussi à comprendre la logique?? ça m'a l'air pas mal du tout

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ * Return: 0 Successful
+ */
+
+int main(void)
+{
+int a, b, c, compteur;
+
+a = 1;
+b = 2;
+c = (a + b);
+compteur = 0;
+
+while (compteur != 98)
+{
+	printf("%d ,", c);
+	a = b;
+	b = c;
+	c = (b + a);
+	compteur++;
+}
+	printf("\n");
+}

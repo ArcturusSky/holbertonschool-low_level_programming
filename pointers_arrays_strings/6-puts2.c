dd815801204d2998d5f6bfb887ba48447838f7c5
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-28 10:36:44 +0200

    S4P2T6- After looking again, the issue was actually simple. I was dirrectly managing the data in the point, and doing 2 + 2 + 2 etc, leading eventually to an unauthorized space memory (by being 2 + 2 skipping the \0) And so to avoid that, I used an array with it's index at it's place. Managing then the index instead of the pointer itself.

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,13 @@
 
 void puts2(char *str)
 	{
-		while (*str != '\0')
+	int i = 0;
+
+		while (str[i] != '\0')
 			{
-			_putchar(*str);
-			str += 2;
+			_putchar(str[i]);
+			i += 2;
 			}
-	str = "\0";
 	_putchar('\n');
 	return;
 	}

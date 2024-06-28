d68434493a862284c04ae564246e5fd72f32bab4
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-28 10:56:21 +0200

    nouvelle version

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * puts2 - Fonction permettant de print une 2 lettres sur 2
- * @str: string print
- * Return: rien
+ * puts2 - Function to print every other character of a string
+ * @str: The string to be printed
+ * Return: Nothing
  */
 
 void puts2(char *str)
-	{
+{
 	int i = 0;
-		while (str[i] != '\0')
-			{
-				if (str[i] % 2 == 0)
-				{
-					_putchar(str[i]);
-					i++;
-				}
-			}
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
 	_putchar('\n');
 	}

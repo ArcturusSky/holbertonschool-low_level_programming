6744df0dca3c798ac0d5be981739fdb6a7c2b498
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-29 13:42:21 +0200

    S4P2T9 - In this task I had to copy a string into another destination. For that since I had already two pointers, I only had to set "*dest = *src" without forgetting to keep the "*" to be sure that it will copy each characters, then going through with a while loop. Then adding the \0 to the destination with the same process. and then returning Dest

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - Fonction qui sert à copier une string ailleurs
+ * @dest: destination de la copie
+ * @src: source de la copie
+ * Return: dest
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+while (*src != '\0')
+{
+	*dest = *src;
+	dest++;
+	src++;
+}
+*dest = '\0';
+
+return (dest);
+}

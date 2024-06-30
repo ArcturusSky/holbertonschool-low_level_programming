c7f66e299e709b613b89d354b195be3317e06109
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-06-30 11:38:15 +0200

    S4P3T4 - renverser un array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * reverse_array - Fonction permettant d'obtenir un string à l'envers
+ * @a: array à retourner
+ * @n: nombre d'élément de l'array
+ * Return: rien
+ */
+
+void reverse_array(int *a, int n)
+	{
+		int *debut = a;
+		int *fin = a + (n - 1);
+		int temp;
+
+		while (*fin != (n - 1))
+			{
+			fin++;
+			}
+			fin--;
+		while (debut < fin)
+		{
+			temp = *debut;
+			*debut = *fin;
+			*fin = temp;
+			
+			debut++;
+			fin--;
+		}
+	return;
+	}

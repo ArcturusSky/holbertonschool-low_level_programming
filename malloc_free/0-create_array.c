b7e8ad49473ad51cc0d59460b764d2385724b6cc
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-09 10:29:05 +0200

    Corrected version of 0-create array, I forgot the case in which "array" was NULL

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	char *array = malloc(size * sizeof(char));
 	unsigned int index = 0;
 
-	if (size == 0)
+	if (size == 0 || array == NULL)
 	{
 		return (NULL);
 	}
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,6 +3,42 @@
 #include <stdlib.h>
 
 /**
+ * _strncat - Fonction qui sert à concactener deux string
+ * @dest: destination
+ * @src: source
+ * @n: nombre de bytes limite
+ * Return: dest
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+char *depart = dest;
+int count = 0;
+
+	while (*dest != ('\0'))
+	{
+		dest++;
+	}
+	while (*src != '\0' && count < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		count++;
+	}
+
+	if (count < n)
+	{
+	*dest = '\0';
+	}
+
+
+return (depart);
+}
+
+
+/**
  * str_concat - Concatenates two strings while allowing
  * the exact right ammount of memory space
  * @s1: string 1

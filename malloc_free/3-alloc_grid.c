c09f1e24aeecc44c897086a4f2bdfa055b0a4204
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-11 14:05:42 +0200

    S6P1T3 - This task was VERY hard. I worked 3 days on it, to understand how it was working and even after all that and chatGPT help I still have a valgrind issue and I don't understand where is the leak. I've tried everything to verify where the leak could be but I can't find it. Even with ChatGPT or peer helps

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,21 +12,41 @@
 
 int **alloc_grid(int width, int height)
 {
-	int sizeuni = 0;
-	int memorysize = 0;
-	
+	int **array;
+	int ligne, colonne;
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0) /** Vérification des dimensions */
 	{
 		return (NULL);
 	}
+/** Allocation de la mémoire pour chaque lignes du tableau */
+	array = malloc(height * sizeof(int *));
 
-	sizeuni = height * width;
-	memorysize = malloc(sizeuni * sizeof(int));
-	if (memorysize == NULL)
+	if (array == NULL) /** Si l'allocation échoue */
 	{
 		return (NULL);
 	}
-
-	return (memorysize);
+/** Allocation de la mémoire pour chaque colonne dans chaque ligne */
+	for (ligne = 0; ligne < height; ligne++)
+	{
+		array[ligne] = malloc(width * sizeof(int));
+		if (array[ligne] == NULL) /** Si l'allocation échoue */
+		{
+			/** Libération de la mémoire déjà allouée */
+			for (colonne = 0; colonne < ligne; colonne++)
+			{
+				free(array[colonne]);
+			}
+		free(array);
+		return (NULL);
+		}
+	}
+	for (ligne = 0; ligne != height; ligne++) /**Init toutes les cases à 0 */
+	{
+		for (colonne = 0; colonne < width; colonne++)
+		{
+			array[ligne][colonne] = 0;
+		}
+	}
+	return (array);
 }

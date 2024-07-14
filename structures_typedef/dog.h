39181e425eb17d00f6b74282a6769cf91d4b71b8
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-14 17:37:11 +0200

    I forgot the the headerguard

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 /**
  * struct dog - Represent a dog basic attribute
  * @name: String for the dogs's name
  * @age: float for dog's age
  * @owner: String for dog's owner name
- * 
+ *
  * Description: A simple structure to store basic dog data.
  */
 
 struct dog
-{ 
+{
 	char *name;		/** Pointer because it's a string */
 	float age;		/** Float for dog's age */
 	char *owner;	/** String for dog's owner */
 };
+
+#endif

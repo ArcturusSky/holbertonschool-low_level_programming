0810345f58dfa93bdcd88105a9f813d9c44abc0d
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-14 20:47:56 +0200

    S6P3T3 - Typedef use. Nothing particulary difficult

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,12 +10,16 @@
  * Description: A simple structure to store basic dog data.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;		/** Pointer because it's a string */
 	float age;		/** Float for dog's age */
 	char *owner;	/** String for dog's owner */
-};
+} dog_t;/**
+ * Creating an alias for 'struct dog' equal to
+ * "typedef struct dog dog_t;"
+ */
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 	/**

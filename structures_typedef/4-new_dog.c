f3c1fd75faba646d89cb5b163c61f7b7f21adf06
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-14 22:19:39 +0200

    S6P3T4 - The checker didn't like that I use an outside function so I put it directly into my code.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,29 @@
 #include "dog.h"
 #include "math.h"
 #include <stdlib.h>
-#include "main.h"
+
+/**
+ * _strcpy - Fonction qui sert à copier une string ailleurs
+ * @dest: destination de la copie
+ * @src: source de la copie
+ * Return: dest
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+char *depart = dest;
+	while (*src != '\0')
+
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+*dest = '\0';
+return (depart);
+}
+
 
 /**
  * new_dog - Function that creates a new dog

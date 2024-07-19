925dd0b6822b8c85580008c70c351fdafd24bc34
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-19 21:53:30 +0200

    S7P2T3 - Fix - the separator is printing now each time the switch works thanks to a flag I set. Each time a case is correct

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -14,26 +14,31 @@ void print_all(const char * const format, ...)
 	va_list PrintAnything;
 	int index = 0;
 	char *TestString;
+	int flagTESTSeparator = 0;
 
 	va_start(PrintAnything, format);
-
 	while (format[index] != '\0')
 	{
 		switch (format[index])
 		{
 			case 'c':
+				flagTESTSeparator = 1;
 				printf("%c", va_arg(PrintAnything, int));
 				break;
 			case 'i':
+				flagTESTSeparator = 1;
 				printf("%d", va_arg(PrintAnything, int));
 				break;
 			case 'f':
+				flagTESTSeparator = 1;
 				printf("%f", va_arg(PrintAnything, double));
 				break;
 			case 's':
+				flagTESTSeparator = 1;
 				TestString = va_arg(PrintAnything, char *);
 				if (TestString == NULL)
 				{
+					flagTESTSeparator = 0;
 					printf("(nil)");
 					break;
 				}
@@ -43,6 +48,10 @@ void print_all(const char * const format, ...)
 				break;
 		}
 		index++;
+		if (flagTESTSeparator == 1 && format[index] != 0)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }

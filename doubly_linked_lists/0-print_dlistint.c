a23370c93d5a7ec0672bbe985f3c50b5d8a577d8
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-31 11:47:09 +0200

    S8P2T0 - Same exercize with Doubly Linked lists.
    - copy the code from Singly Linked Lists first task

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - Prints all the elements of a doubly linked list
+ * @h: a pointer to the head of the list to be printed.
+ *
+ * Return: The number of nodes in the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+	const list_t *currentNode = h;
+		while (currentNode != NULL)
+		{
+			if (currentNode->n == NULL)
+			{
+				printf("");
+				count++;
+				currentNode = currentNode->next;
+			}
+			else
+			{
+				printf("%d\n", currentNode->n);
+				count++;
+				currentNode = currentNode->next;
+			}
+		}
+	}
+
+return (count);
+}

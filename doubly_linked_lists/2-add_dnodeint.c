f8eaaaf98a4eb088fd6f880da1f2e7de961f0626
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-31 13:58:01 +0200

    S8P2T2 - Copy add node code and adapting to double linked list with int (prev node update)

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - function that add a node at the beginning of a dlistint_t
+ * @head: pointer to the pointer to the beginning of the linked list
+ * @n: value to add
+ *
+ * Return: the address of the new elements, or NULL if it failed.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+	dlistint_t *New_Node;
+			/** Allocate memory for the new */
+	New_Node = malloc(sizeof(dlistint_t));
+			/** Check if memory allocation was successful */
+	if (New_Node == NULL)
+	{
+		return (NULL);
+	}
+
+	New_Node->n = n;
+
+	/** Case 1: empty list */
+	if (*head == NULL)
+
+	{
+		*head = New_Node;
+			/** New node become the head and only node */
+		New_Node->prev = NULL;
+		New_Node->next = NULL;
+			/** Since only node, prev and next are NULL */
+	}
+
+	/** Case 2: not empty list */
+	else
+	{
+		New_Node->next = *head;
+				/** Set the next node pointer to head */
+		New_Node->prev = NULL;
+				/** Set the prev node pointer to NULL since first node */
+		*head = New_Node;
+			/** Update the head pointer to point to the new node and become de header */
+	}
+	return (New_Node);
+}

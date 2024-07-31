4a6659235b5c3885c4e76c709572a0098fcdda7a
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-31 14:03:15 +0200

    Fix - Set the head prev node to new node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -27,22 +27,24 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head == NULL)
 
 	{
-		*head = New_Node;
-			/** New node become the head and only node */
 		New_Node->prev = NULL;
 		New_Node->next = NULL;
 			/** Since only node, prev and next are NULL */
+		*head = New_Node;
+			/** New node become the head and only node */
 	}
 
 	/** Case 2: not empty list */
 	else
 	{
+		head->prev = *New_Node;
+				/** Set the head prev node to new node */
 		New_Node->next = *head;
 				/** Set the next node pointer to head */
 		New_Node->prev = NULL;
 				/** Set the prev node pointer to NULL since first node */
 		*head = New_Node;
-			/** Update the head pointer to point to the new node and become de header */
+				/** Update the head pointer to point to the new node and become head */
 	}
 	return (New_Node);
 }

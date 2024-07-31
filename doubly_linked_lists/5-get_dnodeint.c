4d746ef043b5e130e989e7d8e7f6741f6942bfb4
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-31 15:16:22 +0200

    S8P2T5 - Iteration trough the list, set a temporary index to 0 to increase until reaching index or NULL

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_dnodeint_at_index - get node in a specific index in a doubly linked list
+ * @head: pointer to the head of the list
+ * @index: emplacement of the node
+ *
+ * Return: node or NULL if NULL
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int increasing_index = 0;	/**Set a temporary index to keep track */
+
+	if (index == 0 || head == NULL)
+	{
+		return (NULL);					/** return an error */
+	}
+	else
+	{
+
+	dlistint_t *currentNode = head;
+										/** Set a temporary node to hold the head pointer to traverse */
+	while (currentNode->next != NULL)
+										/** Iterrate until While not reaching index  or null */
+	{
+		currentNode = currentNode->next;
+		increasing_index++;
+		if (increasing_index == index)
+		{
+			return (currentNode);		/** If index found return current Node */
+		}
+	}
+	}
+return (NULL);
+}

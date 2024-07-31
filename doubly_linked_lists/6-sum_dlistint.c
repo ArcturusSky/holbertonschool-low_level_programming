7345ac21e1d91deb9b47931a293ccd11d570eb50
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-31 15:36:51 +0200

    S8P2T6 - Iteration trough the list and add the value of currentnode->value each time until end

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_dlistint - sum all "n" in doubly linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: Sum or 0
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum_of_data = 0;
+	dlistint_t *currentNode = head;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	while (currentNode->next != NULL)
+	{
+		currentNode = currentNode->next;
+		sum_of_data += currentNode->n;
+	}
+
+return (sum_of_data);
+}

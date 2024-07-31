e9c06209176c141a545057adbdbbff62cf9085a5
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-07-31 13:14:38 +0200

    Adding the prototype: size_t dlistint_len(const dlistint_t *h);

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -21,4 +21,7 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 	/** Function that prints all elements of a doubly linked lists */
 
+size_t dlistint_len(const dlistint_t *h);
+	/** Function that counts all elements of a doubly linked lists */
+
 #endif

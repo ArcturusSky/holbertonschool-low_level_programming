06c972f55a6550358eae4a1fb751b32796f44a39
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-08-01 13:31:46 +0200

    fix - moving place for the moving to next node part

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,36 +17,41 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *New_Node;
 	dlistint_t *Temporary_Prev_Node;
 
-	if (h == NULL)
-		return (NULL);					/** return an error */
+	if (h == NULL)	/* Check for invalid head pointer */
+		return (NULL);
 
-	if (idx == 0)		/** means index is 0 then return head */
-		return (add_dnodeint(&*h, n));
+	if (idx == 0)	/* Special case: Inserting at the head */
+		return (add_dnodeint(h, n));
 
-	while (currentNode != NULL)	/** Iterrate */
+	while (currentNode != NULL) /* Traverse the list to find the insertion point */
 	{
-		Temporary_Prev_Node = currentNode;
-		currentNode = currentNode->next;
-		increasing_index++;
 		if (increasing_index == idx)
 		{
+			/** Special case: Inserting at the end of the list */
 			if (currentNode->next == NULL)
-			{
 				return (add_dnodeint_end(&*h, n));
-			}
+
+			/** Allocate memory for new node */
 			New_Node = malloc(sizeof(dlistint_t));
 			if (New_Node == NULL)
-			{
 				return (NULL);
-			}
+
+			/** Update pointers for insertion */
 			New_Node->n = n;
 			New_Node->prev = Temporary_Prev_Node;
 			New_Node->next = currentNode;
 			Temporary_Prev_Node->next = New_Node;
 			currentNode->prev = New_Node;
-			return (New_Node);			/** If index found return current Node */
-			}
-	}
 
-	return (NULL);
+			/** If index found return current Node */
+			return (New_Node);
+			}
+
+		/** Move to the next node */
+		Temporary_Prev_Node = currentNode;
+		currentNode = currentNode->next;
+		increasing_index++;
+	}
+	return (NULL);	/** If index is out of bounds, return NULL */
+
 }

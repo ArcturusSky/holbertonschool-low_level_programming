3a29f6c5eefb68590ab0ee537ba53ff31ab04bae
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-08-01 14:21:10 +0200

    fix - test with Fassih code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int increasing_index = 0;	/**Set a temporary index to keep track */
+	unsigned int increasing_index;	/**Set a temporary index to keep track */
 	dlistint_t *currentNode = *h;
 	dlistint_t *New_Node;
 	dlistint_t *Temporary_Prev_Node;
@@ -23,34 +23,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)	/* Special case: Inserting at the head */
 		return (add_dnodeint(h, n));
 
-	while (currentNode != NULL) /* Traverse the list to find the insertion point */
-	{
-		if (increasing_index == idx)
-		{
-			/** Special case: Inserting at the end of the list */
-			if (currentNode->next == NULL)
-				return (add_dnodeint_end(&*h, n));
-
-			/** Allocate memory for new node */
-			New_Node = malloc(sizeof(dlistint_t));
-			if (New_Node == NULL)
-				return (NULL);
-
-			/** Update pointers for insertion */
-			New_Node->n = n;
-			New_Node->prev = Temporary_Prev_Node;
-			New_Node->next = currentNode;
-			Temporary_Prev_Node->next = New_Node;
-			currentNode->prev = New_Node;
-
-			/** If index found return current Node */
-			return (New_Node);
-			}
-
+	for (increasing_index = 0; increasing_index < idx
+	&& currentNode != NULL; increasing_index++)
 		/** Move to the next node */
 		Temporary_Prev_Node = currentNode;
 		currentNode = currentNode->next;
-		increasing_index++;
+	}
+			/** Special case: Inserting at the end of the list */
+	if (currentNode->next == NULL)
+		return (add_dnodeint_end(&*h, n));
+
+	if (increasing_index == idx)	/** Allocate memory for new node */
+	{
+		New_Node = malloc(sizeof(dlistint_t));
+		if (New_Node == NULL)
+			return (NULL);
+
+		/** Update pointers for insertion */
+		New_Node->n = n;
+		New_Node->prev = Temporary_Prev_Node;
+		New_Node->next = currentNode;
+		Temporary_Prev_Node->next = New_Node;
+		currentNode->prev = New_Node;
+
+		/** If index found return current Node */
+		return (New_Node);
 	}
 	return (NULL);	/** If index is out of bounds, return NULL */
 

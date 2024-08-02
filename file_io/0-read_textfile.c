434761b915fbea630fc6081c7d55fde441129385
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-08-02 20:15:41 +0200

    fix - malloc buffer size with letters size.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>   /** Includes definitions for file control operations */
 #include <unistd.h>  /** Includes definitions for POSIX constants and types */
 #include <stdio.h>   /** Includes definitions for standard I/O operations */
+#include <stdlib.h> /** Includes definitions for mem management operations */
 
 /**
  * read_textfile - Function that reads a text and print it
@@ -13,30 +14,42 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fileDescriptor;
-	char buffer[256];
-	ssize_t bytes_to_read;
-	ssize_t bytes_written;
+	char *buffer;
+	ssize_t bytes_to_read, bytes_written;
 
 	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
 		return (0);
 
 	fileDescriptor = open(filename, O_RDONLY);
 
 	if (fileDescriptor < 0)
-		return (0);
-
-	while ((bytes_to_read = read(fileDescriptor, buffer, sizeof((buffer))) > 0))
 	{
-		bytes_written = write(STDOUT_FILENO, buffer, bytes_to_read);
+		free(buffer);
+		return (0);
 	}
 
-	if (bytes_to_read || bytes_written < 0)
+	bytes_to_read = read(fileDescriptor, buffer, sizeof((buffer)));
+	if (bytes_to_read < 0)
 	{
 		close(fileDescriptor);
+		free(buffer);
 		return (0);
 	}
-	letters = bytes_written;
-	close(fileDescriptor);
 
-	return (letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_to_read);
+	if (bytes_written < 0)
+	{
+		close(fileDescriptor);
+		free(buffer);
+		return (0);
+	}
+
+	close(fileDescriptor);
+	free(buffer);
+
+	return (bytes_written);
 }

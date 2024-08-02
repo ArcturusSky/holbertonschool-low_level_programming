03a826d12e5b46ee4d7d77ae43b9961183bafd5e
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-08-02 19:57:42 +0200

    S8P4T0 - First time using File I/O to read a document. It's quite difficult to apply what I saw in my lesson

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,1 +1,42 @@
-ssize_t read_textfile(const char *filename, size_t letters);
+#include <fcntl.h>   /** Includes definitions for file control operations */
+#include <unistd.h>  /** Includes definitions for POSIX constants and types */
+#include <stdio.h>   /** Includes definitions for standard I/O operations */
+
+/**
+ * read_textfile - Function that reads a text and print it
+ * @filename: where to print
+ * @letters: number of letters it should read and print
+ *
+ * Return: size char to read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fileDescriptor;
+	char buffer[256];
+	ssize_t bytes_to_read;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	fileDescriptor = open(filename, O_RDONLY);
+
+	if (fileDescriptor < 0)
+		return (0);
+
+	while ((bytes_to_read = read(fileDescriptor, buffer, sizeof((buffer))) > 0))
+	{
+		bytes_written = write(STDOUT_FILENO, buffer, bytes_to_read);
+	}
+
+	if (bytes_to_read || bytes_written < 0)
+	{
+		close(fileDescriptor);
+		return (0);
+	}
+	letters = bytes_written;
+	close(fileDescriptor);
+
+	return (letters);
+}
